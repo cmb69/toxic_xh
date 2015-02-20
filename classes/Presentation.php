1c1f52fcd0b18d4f5903966e5c316786a7c5c05f
Christoph M. Becker <cmbecker69@gmx.de>
2015-02-20 22:26:24 +0100

    bumped copyright year

--- classes/Presentation.php
+++ classes/Presentation.php
@@ -8,7 +8,7 @@
  * @category  Testing
  * @package   Toxic
  * @author    Christoph M. Becker <cmbecker69@gmx.de>
- * @copyright 2014 Christoph M. Becker <http://3-magi.net>
+ * @copyright 2014-2015 Christoph M. Becker <http://3-magi.net>
  * @license   http://www.gnu.org/licenses/gpl-3.0.en.html GNU GPLv3
  * @link      http://3-magi.net/?CMSimple_XH/Toxic_XH
  */
@@ -337,7 +337,7 @@ class Toxic_InfoCommand
      */
     private function _renderCopyright()
     {
-        return '<p>Copyright &copy; 2014'
+        return '<p>Copyright &copy; 2014-2015'
             . ' <a href="http://3-magi.net/">Christoph M. Becker</a>';
     }
 
--- tests/unit/LiTest.php
+++ tests/unit/LiTest.php
@@ -8,7 +8,7 @@
  * @category  Testing
  * @package   Toxic
  * @author    The CMSimple_XH developers <devs@cmsimple-xh.org>
- * @copyright 2014 The CMSimple_XH developers <http://cmsimple-xh.org/?The_Team>
+ * @copyright 2014-2015 The CMSimple_XH developers <http://cmsimple-xh.org/?The_Team>
  * @license   http://www.gnu.org/licenses/gpl-3.0.en.html GNU GPLv3
  * @link      http://3-magi.net/?CMSimple_XH/Toxic_XH
  */
