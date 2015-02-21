4e18a1b919eb53a0a19ef411977f7fbfb5f0cc7e
Christoph M. Becker <cmbecker69@gmx.de>
2015-02-21 17:54:18 +0100

    improved plugin info view

--- classes/InfoCommand.php
+++ classes/InfoCommand.php
@@ -102,10 +102,10 @@ class Toxic_InfoCommand
 redistribute it and/or modify it under the terms of the GNU General Public
 License as published by the Free Software Foundation, either version 3 of the
 License, or (at your option) any later version.</p>
-<p class="toxic_license">This program is distributed in the hope that it
-will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHAN&shy;TABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General
-Public License for more details.</p>
+<p class="toxic_license">This program is distributed in the hope that it will be
+useful, but <em>without any warranty</em>; without even the implied warranty of
+<em>merchantability</em> or <em>fitness for a particular purpose</em>. See the
+GNU General Public License for more details.</p>
 <p class="toxic_license">You should have received a copy of the GNU
 General Public License along with this program. If not, see <a
 href="http://www.gnu.org/licenses/">http://www.gnu.org/licenses/</a>.</p>
