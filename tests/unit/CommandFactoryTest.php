fdd73931fbeed1266c5271195e8d4213dfb86589
Christoph M. Becker <cmbecker69@gmx.de>
2025-05-07 14:19:35 +0200

    Switch to GNU style file headers
    We drop them altogether for test files (won't ship these anyway).

--- tests/unit/CommandFactoryTest.php
+++ tests/unit/CommandFactoryTest.php
@@ -4,19 +4,6 @@ namespace Toxic;
 
 use PHPUnit\Framework\TestCase;
 
-/**
- * Testing the command factory.
- *
- * PHP version 5
- *
- * @category  Testing
- * @package   Toxic
- * @author    Christoph M. Becker <cmbecker69@gmx.de>
- * @copyright 2014-2015 Christoph M. Becker <http://3-magi.net>
- * @license   http://www.gnu.org/licenses/gpl-3.0.en.html GNU GPLv3
- * @link      http://3-magi.net/?CMSimple_XH/Toxic_XH
- */
-
 class CommandFactoryTest extends TestCase
 {
     /** @var CommandFactory  */
--- tests/unit/TabCommandTest.php
+++ tests/unit/TabCommandTest.php
@@ -5,19 +5,6 @@ namespace Toxic;
 use ApprovalTests\Approvals;
 use PHPUnit\Framework\TestCase;
 
-/**
- * Testing the tab command.
- *
- * PHP version 5
- *
- * @category  Testing
- * @package   Toxic
- * @author    Christoph M. Becker <cmbecker69@gmx.de>
- * @copyright 2014-2015 Christoph M. Becker <http://3-magi.net>
- * @license   http://www.gnu.org/licenses/gpl-3.0.en.html GNU GPLv3
- * @link      http://3-magi.net/?CMSimple_XH/Toxic_XH
- */
-
 class TabCommandTest extends TestCase
 {
     /** @var TabCommand */
